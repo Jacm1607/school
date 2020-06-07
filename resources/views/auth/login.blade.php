26e7711d12770b09ab105d5f4f9b3d86590d62c6
Jacm1607 <chavarriaj84@gmail.com>
2020-06-07 13:26:24 -0400

    v1.2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,40 @@
-@extends('layouts.app')
+@extends('template.tp1')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+<div class="error-pagewrap">
+    <div class="error-page-int">
+        <div class="text-center m-b-md custom-login">
+            <h3>Iniciar Sesión</h3>
+            <p>iEdu</p>
+            <img src="https://api.adorable.io/avatars/285/abott@adorable.png" style="width: 150px; height: 150px; border-radius: 16%;" alt="">
+        </div>
+        <div class="content-error">
+            <div class="hpanel">
+                <div class="panel-body">
+                    <form  method="POST" action="{{ route('login') }}" autocomplete="off">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="form-group">
+                            <label class="control-label" for="username">Username</label>
+                            <input type="email" placeholder="usuario@ejemplo.com" required="" name="email" value="{{ old('email') }}" autofocus class="form-control form-control @error('email') is-invalid @enderror">
+                            @error('email')
+                                <span class="help-block small" style="color:#E74C3C"><strong>{{ $message }}</strong></span>
+                            @enderror
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="form-group">
+                            <label class="control-label" for="password">Password</label>
+                            <input type="password" title="Please enter your password" placeholder="*********" required=""  autocomplete="current-password" value="" name="password" id="password" class="form-control @error('password') is-invalid @enderror">
+                            @error('password')
+                                <span class="help-block small"><strong>{{ $message }}</strong></span>
+                            @enderror
                         </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
+                        <button class="btn btn-success btn-block loginbtn">Login</button>
                     </form>
                 </div>
             </div>
         </div>
-    </div>
+        <div class="text-center login-footer">
+            <p>Copyright © @php echo date("Y"); @endphp. All rights reserved. <a href="https://www.facebook.com/Consulogy-104196401305227/">Consulogy</a></p>
+        </div>
+    </div>   
 </div>
 @endsection
---
+++ resources/views/template/tp1.blade.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html class="no-js" lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>iEdu | U.E.</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- favicon
+		============================================ -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('template/img/favicon.ico')}}">
+    <!-- Google Fonts
+		============================================ -->
+    <!-- <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet"> -->
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/css/bootstrap.min.css')}}">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"> -->
+    <!-- Bootstrap CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/css/font-awesome.min.css')}}">
+    <!-- owl.carousel CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/css/owl.carousel.css')}}">
+    <link rel="stylesheet" href="{{asset('template/css/owl.theme.css')}}">
+    <link rel="stylesheet" href="{{asset('template/css/owl.transitions.css')}}">
+    <!-- animate CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/css/animate.css')}}">
+    <!-- normalize CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/css/normalize.css')}}">
+    <!-- meanmenu icon CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/css/meanmenu.min.css')}}">
+    <!-- main CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/css/main.css')}}">
+    <!-- educate icon CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/css/educate-custon-icon.css')}}">
+    <!-- morrisjs CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/css/morrisjs/morris.css')}}">
+    <!-- mCustomScrollbar CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/css/scrollbar/jquery.mCustomScrollbar.min.css')}}">
+    <!-- metisMenu CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/css/metisMenu/metisMenu.min.css')}}">
+    <link rel="stylesheet" href="{{asset('template/css/metisMenu/metisMenu-vertical.css')}}">
+    <!-- calendar CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/css/calendar/fullcalendar.min.css')}}">
+    <link rel="stylesheet" href="{{asset('template/css/calendar/fullcalendar.print.min.css')}}">
+    <!-- style CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/style.css')}}">
+    <!-- responsive CSS
+		============================================ -->
+    <link rel="stylesheet" href="{{asset('template/css/responsive.css')}}">
+    <!-- modernizr JS
+		============================================ -->
+    <script src="{{asset('template/js/vendor/modernizr-2.8.3.min.js')}}"></script>
+</head>
+
+<body>
+    @yield('content')
+    <!-- jquery
+		============================================ -->
+    <script src="{{asset('template/js/vendor/jquery-1.12.4.min.js')}}"></script>
+    <!-- bootstrap JS
+		============================================ -->
+    <script src="{{asset('template/js/bootstrap.min.js')}}"></script>
+    <!-- wow JS
+		============================================ -->
+    <script src="{{asset('template/js/wow.min.js')}}"></script>
+    <!-- price-slider JS
+		============================================ -->
+    <script src="{{asset('template/js/jquery-price-slider.js')}}"></script>
+    <!-- meanmenu JS
+		============================================ -->
+    <script src="{{asset('template/js/jquery.meanmenu.js')}}"></script>
+    <!-- owl.carousel JS
+		============================================ -->
+    <script src="{{asset('template/js/owl.carousel.min.js')}}"></script>
+    <!-- sticky JS
+		============================================ -->
+    <script src="{{asset('template/js/jquery.sticky.js')}}"></script>
+    <!-- scrollUp JS
+		============================================ -->
+    <script src="{{asset('template/js/jquery.scrollUp.min.js')}}"></script>
+    <!-- counterup JS
+		============================================ -->
+    <script src="{{asset('template/js/counterup/jquery.counterup.min.js')}}"></script>
+    <script src="{{asset('template/js/counterup/waypoints.min.js')}}"></script>
+    <script src="{{asset('template/js/counterup/counterup-active.js')}}"></script>
+    <!-- mCustomScrollbar JS
+		============================================ -->
+    <script src="{{asset('template/js/scrollbar/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+    <script src="{{asset('template/js/scrollbar/mCustomScrollbar-active.js')}}"></script>
+    <!-- metisMenu JS
+		============================================ -->
+    <script src="{{asset('template/js/metisMenu/metisMenu.min.js')}}"></script>
+    <script src="{{asset('template/js/metisMenu/metisMenu-active.js')}}"></script>
+    <!-- morrisjs JS
+		============================================ -->
+    <script src="{{asset('template/js/morrisjs/raphael-min.js')}}"></script>
+    <script src="{{asset('template/js/morrisjs/morris.js')}}"></script>
+    <script src="{{asset('template/js/morrisjs/morris-active.js')}}"></script>
+    <!-- morrisjs JS
+		============================================ -->
+    <script src="{{asset('template/js/sparkline/jquery.sparkline.min.js')}}"></script>
+    <script src="{{asset('template/js/sparkline/jquery.charts-sparkline.js')}}"></script>
+    <script src="{{asset('template/js/sparkline/sparkline-active.js')}}"></script>
+    <!-- calendar JS
+		============================================ -->
+    <script src="{{asset('template/js/calendar/moment.min.js')}}"></script>
+    <script src="{{asset('template/js/calendar/fullcalendar.min.js')}}"></script>
+    <script src="{{asset('template/js/calendar/fullcalendar-active.js')}}"></script>
+    <!-- plugins JS
+		============================================ -->
+    <script src="{{asset('template/js/plugins.js')}}"></script>
+    <!-- main JS
+		============================================ -->
+    <script src="{{asset('template/js/main.js')}}"></script>
+    <!-- tawk chat JS
+		============================================ -->
+    <!-- <script src="{{asset('template/js/tawk-chat.js')}}"></script> -->
+</body>
+
+</html>
